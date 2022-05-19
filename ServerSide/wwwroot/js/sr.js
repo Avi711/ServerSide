62d1c4b6b567c9ce0125dce83592d2e33ce38215
Avi <avraham.sikirov@live.biu.ac.il>
2022-05-19 19:30:48 +0300

    signalR

---
+++ ServerSide/wwwroot/js/sr.js
@@ -0,0 +1,15 @@
+﻿$(function () {
+    var connection = new signalR.hubConnectionBuilder().writeUrl("/myHub").build();
+
+    connection.start();
+
+    $('message-input-form').submit(() => {
+        console.log('sending: ' + $('textarea').val());
+        connection.invoke("Changed", $('textarea').val());
+    });
+
+    connection.on("ChangedRecived", function (value) {
+        console.log('recieved: ' + value);
+        $('textarea').val(value);
+    });
+});
